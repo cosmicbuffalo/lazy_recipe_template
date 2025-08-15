cfc57df1a335c2557a1aac19944bd4e88ec1d789
Nick deLannoy <nick.delannoy@gmail.com>
2025-08-15 16:45:18 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# My Lazy Recipe
+
+A starter **lazy.nvim recipe** that bundles one or multiple customized plugin configurations into a single lazy spec
+
+# 📦 Installation
+
+[Lazy.nvim](https://github.com/folke/lazy.nvim):
+
+```lua
+return {
+  "github_username/this_repo",
+}
+```
---
+++ lazy.lua
@@ -0,0 +1,27 @@
+-- lazy.lua
+-- This file is automatically picked up by lazy.nvim when this recipe repo is
+-- loaded as a plugin. Any plugins and configurations can be defined here the
+-- samy way as they would be in a normal lazy configuration file.
+return {
+  -- Optional: give this recipe a unique name (defaults to repo name)
+  -- name = "my-awesome-recipe",
+
+  -- Optional: run code when the recipe is loaded
+  -- init = function()
+  --   vim.notify("Loading my-awesome-recipe", vim.log.levels.INFO, { title = "Lazy.nvim" })
+  -- end
+  
+  -- Include plugin configurations here
+  -- {
+  --   "example/plugin",
+  --   opts = {
+  --     -- customized plugin options
+  --   }
+  -- },
+  -- {
+  --   "another/plugin",
+  --   opts = {
+  --     -- more plugin options
+  --   }
+  -- }
+}
